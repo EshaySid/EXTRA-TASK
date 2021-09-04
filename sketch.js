082de2170605393e6b936c781f970d58269bba45
EshaySid <85012381+EshaySid@users.noreply.github.com>
2021-09-04 18:16:59 +0930

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+function setup() {
+  createCanvas(800,800);
+/*   for(var i=1; i<=100; i++){
+    if(i%2==0){
+      console.log(i);
+    }
+  } */
+/*   for(var i=100; i<200; i++){
+    if(i%5==0){
+      console.log(i);
+    }
+  } */
+/*   for(var i=1; i<=100; i++){
+    if(i%10==7||i%7==0){
+      console.log(i);
+    }
+  } */
+    for(var i=1; i<=144; i++){
+      if(144%i==0){
+        console.log(i);
+      }
+    }
+}
+
+function draw() {
+  background("black");  
+ 
+  
+}
+
+
+
+
